087678e0b97ae79dd0fa76a28e867374e4606560
CookieeQuinn <lxq1191868132@gmail.com>
2026-07-13 23:27:14 +0800

    chap(12-18): Assets, Metadata, Script and CSS Styling

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Layout from "../../components/layout";
 import Head from "next/head";
+// import Script from "next/script";
 
 function FirstPost() {
   return (
@@ -8,6 +9,14 @@ function FirstPost() {
       <Head>
         <title>First Post</title>
       </Head>
+      {/* 这个 Facebook 的 SDK 只是作为例子演示如何使用 Script 组件引入第三方 JS */}
+      {/* <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() => {
+          console.log("load script");
+        }}
+      /> */}
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to Home</Link>
